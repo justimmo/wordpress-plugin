ffe55312d88c4bd3ab6ed96e8b288e56af2932c4
Mihalache Razvan <mrazvani@yahoo.com>
2016-10-12 23:50:48 +0300

    update project realties partial: transform unordered list into table

--- public/partials/project/_project-info__realties.php
+++ public/partials/project/_project-info__realties.php
@@ -1,20 +1,69 @@
 <?php $realties_array = $project->getRealties(); ?>
 
-<?php if ( count($realties_array) ): ?>
+<?php if (count($realties_array)) : ?>
 
     <section class="ji-realty-list ji-realty-list--project">
 
-        <h1 class="ji-realty-list__title"><?php echo _e( 'Project Realties:', 'jiwp' ); ?></h1>
+        <h1 class="ji-realty-list__title"><?php echo _e('Project Realties:', 'jiwp'); ?></h1>
 
-        <ul class="ji-realty-list">
+        <table class="project-realties">
+            <thead>
+                <tr>
+                    <th><?php echo _e('Nb.', 'jiwp'); ?></th>
+                    <th><?php echo _e('Floor', 'jiwp'); ?></th>
+                    <th><?php echo _e('Living Area', 'jiwp'); ?></th>
+                    <th><?php echo _e('Room Count', 'jiwp'); ?></th>
+                    <th><?php echo _e('Balcony Surface', 'jiwp'); ?></th>
+                    <th><?php echo _e('Terrace Surface', 'jiwp'); ?></th>
+                    <th><?php echo _e('Garden Surface', 'jiwp'); ?></th>
+                    
+                    <?php $marketingType = $realties_array[0]->getMarketingType(); ?>
 
-            <?php foreach ( $realties_array as $realty ): ?>
+                    <?php if ($marketingType['KAUF'] == true) : ?>
+                        <th><?php echo _e('Purchase Price', 'jiwp'); ?></th>
+                    <?php else : ?>
+                        <th><?php echo _e('Rent', 'jiwp'); ?></th>
+                    <?php endif; ?>
 
-                <?php include( Jiwp_Public::get_template( 'realty/_realty-list__item.php' ) ); ?>
+                    <th><?php echo _e('Status', 'jiwp'); ?></th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php foreach ($realties_array as $index => $realty) : ?>
+                    <tr data-href="<?php echo get_bloginfo('url') . '/realties/' . $realty->getId() ?>">
+                        <td>
+                            <a href="<?php echo get_bloginfo('url') . '/realties/' . $realty->getId() ?>">
+                                <?php echo $index + 1; ?>
+                            </a>
+                        </td>
+                        <td><?php echo $realty->getTier(); ?></td>
+                        <td><?php echo $realty->getLivingArea(); ?></td>
+                        <td><?php echo $realty->getRoomCount(); ?></td>
+                        <td><?php echo $realty->getBalconyArea(); ?></td>
+                        <td><?php echo $realty->getTerraceArea(); ?></td>
+                        <td><?php echo $realty->getGardenArea(); ?></td>
+                        <td>
+                            <?php
 
-            <?php endforeach; ?>
+                            $currency = $realty->getCurrency();
 
-        </ul>
+                            if (empty($currency)) {
+                                $currency = 'EUR';
+                            }
+
+                            if ($marketingType['KAUF'] == true) {
+                                echo money_format("%!i $currency", $realty->getPurchasePrice());
+                            } else {
+                                echo money_format("%!i $currency", $realty->getTotalRent());
+                            }
+
+                            ?>
+                        </td>
+                        <td><?php echo $realty->getStatus(); ?></td>
+                    </tr>
+                <?php endforeach; ?>
+            </tbody>
+        </table>
 
     </section>
 
