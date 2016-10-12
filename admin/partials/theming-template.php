e5e00b67af50309fa3f7d5d1824001a287eff15a
Mihalache Razvan <mrazvani@yahoo.com>
2016-10-12 23:51:24 +0300

    update theming panel with new template options

--- admin/partials/theming-template.php
+++ admin/partials/theming-template.php
@@ -1,25 +1,40 @@
-<?php /*
 <h3><?php _e( 'How to customize templates and styles', 'jiwp' ); ?></h3>
 
-<p><?php _e( 'To overwrite a template, create a folder named <code>jiwp-templates</code> inside your active theme\'s folder in which you should place a copy of that template (with the same name), modifying it to meet your requirements.', 'jiwp' ); ?></p>
+<p><?php _e( 'To overwrite a template, create a folder named <code>jiwp-templates</code> in your active theme\'s folder in which you should place a copy of that template along with it\'s parent folder if required.', 'jiwp' ); ?></p>
 
-<p><?php _e( ' Here is a list of templates that can be overwritten:', 'jiwp' ); ?></p>
+<p><?php _e( 'Here is a list of templates that can be overwritten:', 'jiwp' ); ?></p>
 
 <ol class="ji-template-list">
+	
 	<li class="ji-template">
-		<code>realty-template.php</code>
+		<code>project/project-template.php</code>
 	</li>
 	<li class="ji-template">
-		<code>search-results-template.php</code>
+		<code>project/_project-list.php</code>
 	</li>
 	<li class="ji-template">
-		<code>_realty-list.php</code>
+		<code>project/_project-list__item.php</code>
 	</li>
 	<li class="ji-template">
-		<code>_realty-info.php</code>
+		<code>project/_project-info.php</code>
 	</li>
 	<li class="ji-template">
-		<code>_realty-extended-info.php</code>
+		<code>realty/realty-template.php</code>
+	</li>
+	<li class="ji-template">
+		<code>realty/_realty-list.php</code>
+	</li>
+	<li class="ji-template">
+		<code>realty/_realty-list__item.php</code>
+	</li>
+	<li class="ji-template">
+		<code>realty/_realty-info.php</code>
+	</li>
+	<li class="ji-template">
+		<code>realty/_realty-extended-info.php</code>
+	</li>
+	<li class="ji-template">
+		<code>search-form/search-results-template.php</code>
 	</li>
 	<li class="ji-template">
 		<code>_pagination.php</code>
@@ -27,6 +42,3 @@
 </ol>
 
 <p><?php _e( 'You can find the templates listed above inside the <code>/public/partials</code> folder in your plugin folder', 'jiwp' ); ?></p>
-*/ ?>
-
-Coming soon
