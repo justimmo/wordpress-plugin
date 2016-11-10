824f122914d6f3934791807991241ab25966da8b
Mihalache Razvan <mrazvani@yahoo.com>
2016-11-10 15:41:06 +0200

    add fallback for no title case

--- public/partials/realty/realty-template.php
+++ public/partials/realty/realty-template.php
@@ -6,7 +6,14 @@
 
         <header class="ji-realty__header">
 
-            <h1 class="ji-realty__title"><?php echo $realty->getTitle(); ?></h1>
+            <h1 class="ji-realty__title">
+                <?php $realty_title = $realty->getTitle(); ?>
+                <?php if (!empty($realty_title)) : ?>
+                    <?php echo $realty->getTitle(); ?>
+                <?php else : ?>
+                    <?php echo $realty->getRealtyTypeName() . ' ' . __('in', 'jiwp') . ' ' . $realty->getCountry() . ' / ' . $realty->getFederalState(); ?>
+                <?php endif; ?>
+            </h1>
 
         </header>
 
